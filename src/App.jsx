4470753f4399290e1a16e69611506d87b0e9f76e
daxho <developer.daxho@gmail.com>
2025-06-10 21:40:51 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ToggleNavButton from "./ToggleNavButton";
+
+export default function App() {
+  return (
+    <div className="app">
+      <header className="main-header">
+        <h1 className="brand">What's Next?</h1>
+        <ToggleNavButton />
+      </header>
+    </div>
+  );
+}
---
+++ src/ToggleNavButton.jsx
@@ -0,0 +1,9 @@
+export default function ToggleNavButton() {
+  return (
+    <button className="toggle-nav-button">
+      <span className="button-bar"></span>
+      <span className="button-bar"></span>
+      <span className="button-bar"></span>
+    </button>
+  );
+}
